b3304af02157ef85594531dd470ffc519ca8455e
Fantin Sibony <fantinsibony@gmail.com>
2026-01-05 22:23:42 +0100

    Remove image.png from root

---
+++ test/test_option_chain_generator.py
@@ -0,0 +1,11 @@
+###### tests for option chain data generation 
+
+
+import sys
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+import option_chain_generator as oc
+
